b3d77d9d4a00bc9d875240168d8edf2e18227f53
lilinj2000 <lilinj2000@gmail.com>
2015-09-30 16:01:16 +0800

    0930 data collection

--- xele_data/xele_data.m
+++ xele_data/xele_data.m
@@ -11,15 +11,20 @@
 
 xele_net_timestamp = char(xele_net(:, 1));
 zeusing_udp_timestamp = char(zeusing_udp(:, 1));
+% fm8 tcp v1_01
+fm8_v1_01_timestamp = char(fm8_v1_01(:, 1));
 
 
 % common time stamp
 common_time_stamp = intersect(xele_net_timestamp, zeusing_udp_timestamp, 'rows');
+common_time_stamp = intersect(fm8_v1_01_timestamp, common_time_stamp, 'rows');
 
 
 % common data rows
 xele_net_delay = find(ismember(xele_net_timestamp, common_time_stamp, 'rows'));
 zeusing_udp_delay = find(ismember(zeusing_udp_timestamp, common_time_stamp, 'rows'));
+
+fm8_v1_01_delay = find(ismember(fm8_v1_01_timestamp, common_time_stamp, 'rows'));
 
 % construt the result data
 result_delay = cell(0);
@@ -31,8 +36,11 @@ result_delay(:, 2) = xele_net(xele_net_delay, 2);
 % zeusing udp 
 result_delay(:, 3) = zeusing_udp(zeusing_udp_delay, 2);
 
+% fm8 v1_01 tcp
+result_delay(:, 4) = fm8_v1_01(fm8_v1_01_delay, 2);
+
 result_relate_delay = [];
-for ii=2:3
+for ii=2:4
     result_relate_delay(:, ii-1) = cell2mat(result_delay(:, ii)) - cell2mat(result_delay(:, 2));
 end
     
