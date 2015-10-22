79305edba1453dc9e95a1f060a3e4208e84ad6a7
lilinj2000 <lilinj2000@gmail.com>
2015-10-22 13:30:11 +0800

    custom data analysis done.

---
+++ xele_data/custom_data.m
@@ -0,0 +1,13 @@
+
+% raw_data = IC1603;
+% raw_data(:, 2) = num2cell(VarName2);
+% raw_data(:, 3) = num2cell(VarName3);
+% raw_data(:, 4) = num2cell(VarName4);
+
+% result_relate_delay = [];
+% for ii=3:4
+%     result_relate_delay(:, ii-2) = cell2mat(raw_data(:, ii)) - cell2mat(raw_data(:, 3));
+% end
+
+% min_value = min(result_relate_delay(:, 2));
+% max_value = max(result_relate_delay(:, 2));
