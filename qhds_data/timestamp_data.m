51b6c9b980c0daf88793c20dac6aed94850e45e3
lilinj2000 <lilinj2000@gmail.com>
2016-01-26 16:55:39 +0800

    add stat.m

---
+++ qhds_data/timestamp_data.m
@@ -0,0 +1,26 @@
+
+% load data
+
+% zeusing_data = VarName1;
+% zeusing_data(:, 2) = VarName5;
+% zeusing_data(:, 3) = VarName6;
+% 
+% t2_data = zeusing_data(:, 3);
+% 
+% zeusing_stat = mean( t2_data );
+% zeusing_stat(:, 2) = std( t2_data);
+
+xele_data = VarName1;
+xele_data(:, 2) = VarName5;
+xele_data(:, 3) = VarName6;
+
+t2_data = xele_data(:, 3);
+
+xele_stat = mean( t2_data );
+xele_stat(:, 2) = std( t2_data);
+    
+% dfittool
+
+
+
+
