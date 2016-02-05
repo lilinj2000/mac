9436a9bbbac51a0e6374b13d7e42640427ab320b
lilinj2000 <lilinj2000@gmail.com>
2016-02-05 11:42:36 +0800

    analysis done.

--- qhds_data/timestamp_data.m
+++ qhds_data/timestamp_data.m
@@ -20,9 +20,10 @@
 % xele_stat(:, 2) = std( t2_data);
 
 % up delay analysis
-up_delay(:, 3) = VarName4;
-up_delay(:, 2) = VarName4;
-up_delay(:, 1) = VarName4;
+% up_delay(:, 1) = VarName4;
+% up_delay(:, 2) = VarName4;
+% up_delay(:, 3) = VarName4;
+
 
 stat_mean_up = mean(up_delay(:, 1));
 stat_mean_up(2) = mean(up_delay(:, 2));
@@ -30,7 +31,15 @@ stat_mean_up(3) = mean(up_delay(:, 3));
 
 stat_std_up = std(up_delay(:, 1));
 stat_std_up(2) = std(up_delay(:, 2));
-stat_std_up(3) = stad(up_delay(:, 3));
+stat_std_up(3) = std(up_delay(:, 3));
+
+stat_min_up = min(up_delay(:, 1));
+stat_min_up(2) = min(up_delay(:, 2));
+stat_min_up(3) = min(up_delay(:, 3));
+
+stat_max_up = max(up_delay(:, 1));
+stat_max_up(2) = max(up_delay(:, 2));
+stat_max_up(3) = max(up_delay(:, 3));
     
 % dfittool
 
